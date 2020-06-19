d4e4e37e23bb212efdaef34124e7eed86ae85f20
pvgdev <pvgdev@outlook.com>
2020-06-19 04:44:50 +0000

    fix(typings): added typings to repo

---
+++ typings/mdx.d.ts
@@ -0,0 +1,6 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+declare module '*.mdx' {
+  let MDXComponent: (props: any) => JSX.Element
+  export default MDXComponent
+  export const metadata
+}
